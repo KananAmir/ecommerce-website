fe2e87059362f94a1b5a015c471538c8f187c092
NargizMe <59040880+NargizMe@users.noreply.github.com>
2022-10-11 15:23:46 +0400

    feat: container added ton admin layout

--- client/src/layout/LayoutAdmin.jsx
+++ client/src/layout/LayoutAdmin.jsx
@@ -1,15 +1,18 @@
 import { useState } from 'react'
 import {Link} from "react-router-dom";
 import AdminHeader from "../components/admin/admin-header";
+import CssBaseline from '@mui/material/CssBaseline';
+import Container from '@mui/material/Container';
 
 function LayoutAdmin({children}) {
 
     return (
         <>
             <AdminHeader/>
-            <main>
+            <CssBaseline />
+            <Container fixed>
                 {children}
-            </main>
+            </Container>
         </>
     )
 }
