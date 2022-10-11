18451d67e45c23585d4a767bb31bc888fb2dd335
Kanan Amirov <amirovknn@gmail.com>
2022-10-11 11:44:38 +0400

    redux config done

--- client/src/index.js
+++ client/src/index.js
@@ -3,16 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import {BrowserRouter} from "react-router-dom";
+import { applyMiddleware, createStore } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import rootReducer from './redux/reducers'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const store = createStore(rootReducer, applyMiddleware(thunk))
+
 root.render(
   <React.StrictMode>
       <BrowserRouter>
-          <App />
+        <Provider store={store}>
+            <App />
+        </Provider>
       </BrowserRouter>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
