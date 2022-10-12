c3ef716fbad19ec10414bf3c42aa3f85b4362180
NargizMe <59040880+NargizMe@users.noreply.github.com>
2022-10-12 16:46:03 +0400

    feat: categoy added

--- client/src/pages/admin/add-category/index.jsx
+++ client/src/pages/admin/add-category/index.jsx
@@ -1,12 +1,65 @@
-import React from 'react'
+import React from 'react';
+import { Formik, Form, Field } from 'formik';
+import * as Yup from 'yup';
 import LayoutAdmin from "../../../layout/LayoutAdmin";
+import TextField from "@mui/material/TextField";
+import {Button} from "@mui/material";
+
+// const SignupSchema = Yup.object().shape({
+//     name: Yup.string()
+//         .min(2, 'Too Short!')
+//         .max(50, 'Too Long!')
+//         .required('Required'),
+//     surname: Yup.string()
+//         .min(2, 'Too Short!')
+//         .max(50, 'Too Long!')
+//         .required('Required'),
+//     email: Yup.string().email('Invalid email').required('Required'),
+// });
 
 const AddCategory = () => {
     return (
         <LayoutAdmin>
-            Hi I am AddCategory
+            <Formik
+                initialValues={{
+                    name: '',
+                }}
+                // validationSchema={SignupSchema}
+                onSubmit={values => {
+                    // same shape as initial values
+                    console.log(values);
+                }}
+            >
+                {({ errors, touched }) => (
+                    <Form style={formStyle}>
+                        <TextField
+                            required
+                            name="name"
+                            id="outlined-required"
+                            label="Name"
+                        />
+                        {errors.name && touched.name ? (
+                            <div>{errors.name}</div>
+                        ) : null}
+                        <Button
+                            type="submit"
+                            variant="contained"
+                        >
+                            Add
+                        </Button>
+                    </Form>
+                )}
+            </Formik>
         </LayoutAdmin>
     )
 }
 
 export default AddCategory
+
+
+//-----------style-----------
+const formStyle = {
+    width: '300px',
+    display: 'flex',
+    flexDirection: 'column',
+}
