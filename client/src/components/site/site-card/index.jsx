fb0d7fdbcbcb46d7d4a6abc2a1ed414afe1cbc4f
Kanan Amirov <amirovknn@gmail.com>
2022-10-12 22:35:07 +0400

    Feat: product api services handled

--- client/src/components/site/site-card/index.jsx
+++ client/src/components/site/site-card/index.jsx
@@ -1,49 +1,61 @@
-import React from 'react';
-import { Card, CardHeader, CardMedia, CardContent, Avatar, Typography } from '@mui/material';
-import { red } from '@mui/material/colors';
-import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
-import {Link} from "react-router-dom";
+import React from "react";
+import {
+  Card,
+  CardHeader,
+  CardMedia,
+  CardContent,
+  Avatar,
+  Typography,
+} from "@mui/material";
+import { red } from "@mui/material/colors";
+import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
+import { Link } from "react-router-dom";
 
 const SiteCard = (props) => {
-
-    return (
-        <Card style={{width: '345px', minWidth: '175px'}}>
-            <CardHeader
-                avatar={<Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-                    {props.name[0]}
-                        </Avatar>
-                }
-                title={props.name}
-                subheader={props.brandId}
-            />
-                <Link to ={`/product-detail/${props.id}/${props.name}`}>
-                    <CardMedia
-                        component="img"
-                        height="194"
-                        image={props.img}
-                        alt="img"
-                    />
-                </Link>
-            <CardContent>
-                <>
-                    <div style={{display: 'flex',justifyContent: 'space-between', marginBottom: '13px'}}>
-                        <h2>{props.price} AZN</h2>
-                        <AddShoppingCartIcon style={icon}/>
-                    </div>
-                    <Typography variant="body2" color="text.secondary">
-                        {props.name}
-                    </Typography>
-                </>
-            </CardContent>
-        </Card>
-    )
-}
+  return (
+    <Card style={{ width: "345px", minWidth: "175px" }}>
+      <CardHeader
+        avatar={
+          <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+            {props.name[0]}
+          </Avatar>
+        }
+        title={props.name}
+        subheader={props.brandId}
+      />
+      <Link
+        to={`/product-detail/${props.id}/${props.name
+          .toLocaleLowerCase("en-US")
+          .split(" ")
+          .join("-")}`}
+      >
+        <CardMedia component="img" height="194" image={props.img} alt="img" />
+      </Link>
+      <CardContent>
+        <>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              marginBottom: "13px",
+            }}
+          >
+            <h2>{props.price} AZN</h2>
+            <AddShoppingCartIcon style={icon} />
+          </div>
+          <Typography variant="body2" color="text.secondary">
+            {props.name}
+          </Typography>
+        </>
+      </CardContent>
+    </Card>
+  );
+};
 
 export default SiteCard;
 
-
 // --------------style-----------------
 const icon = {
-    color: '#f44336',
-    cursor: 'pointer',
-}
+  color: "#f44336",
+  cursor: "pointer",
+};
