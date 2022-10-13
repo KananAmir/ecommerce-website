d8d7c618eb03ef212f2088d33dc881090d55e654
RashadXalil <rashadmkh@code.edu.az>
2022-10-13 15:51:25 +0400

    orderItem controller created !

--- server/app/controllers/order.controller.js
+++ server/app/controllers/order.controller.js
@@ -1,9 +1,39 @@
-const { Order } = require('../models/order.model')
+const { default: mongoose } = require('mongoose')
+const { Order, default: Order } = require('../models/order.model')
 
 const orderController = {
-  getAll: (req, res) => {},
-  getById: (req, res) => {},
-  add: (req, res) => {},
+  getAll: (req, res) => {
+    Order.find({}, (err, docs) => {
+      if (!err) {
+        res.json(docs)
+      } else {
+        res.status(500).json(err)
+      }
+    })
+  },
+  getById: (req, res) => {
+    let id = req.params.id
+
+    Order.findById(id, (err, docs) => {
+      if (!err) {
+        res.json(docs)
+      } else {
+        res.status(500).json(err)
+      }
+    })
+  },
+  add: (req, res) => {
+    let Order = new Order({
+      userId: mongoose.Types.ObjectId(req.body.userId),
+      orderItems: req.body.orderItems,
+      totalPrice: req.body.totalPrice,
+    })
+    Order.save((err, docs) => {
+      if (!err) {
+        res.send(`Order Created ! ${Order}`)
+      }
+    })
+  },
   edit: (req, res) => {},
   delete: (req, res) => {},
 }
---
+++ server/app/controllers/order.item.controller.js
@@ -0,0 +1,9 @@
+const { OrderItem } = require('../models/orderItem.model')
+
+const orderItemController = {
+  getAll: (req, res) => {},
+  getById: (req, res) => {},
+  add: (req, res) => {},
+  edit: (req, res) => {},
+  delete: (req, res) => {},
+}
