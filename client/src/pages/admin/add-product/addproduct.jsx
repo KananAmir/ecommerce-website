494efa947b9819710ab4c2a02b53e30f623521af
Bahram Jabbarov <bahram.pj@code.local>
2022-10-13 12:17:33 +0400

    post request

--- client/src/pages/admin/add-product/addproduct.jsx
+++ client/src/pages/admin/add-product/addproduct.jsx
@@ -10,23 +10,26 @@ import FormControl from '@mui/material/FormControl';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import TextareaAutosize from '@mui/material/TextareaAutosize';
+import { useForm } from "react-hook-form";
 
 const NewProductForm = () => {
 
-    const { id } = useParams(); 
+    const { register, handleSubmit } = useForm();
 
-    const [category, setCategory] = useState('');
-    const [brand, setBrand] = useState('');
-    const [name, setName] = useState('');
-    const [product, setProduct] = useState({});
-    const [stock, setStock] = useState();
-    const [price, setPrice] = useState();
-    const [description, setDescription] = useState('');
+    const { id } = useParams(); 
 
     if(id){
         axios.get(`/product/${id}`)
         .then( response => setProduct(response.data))
     }
+
+    const [product, setProduct] = useState({});
+    const [category, setCategory] = useState(product.brand ||'');
+    const [brand, setBrand] = useState(product.brand ||'');
+    const [name, setName] = useState( product.name ||'');
+    const [stock, setStock] = useState( product.stock || 0);
+    const [price, setPrice] = useState(product.price || 0);
+    const [description, setDescription] = useState( product.description || '');
 
     const handleChangeCategory = (event) => {
         setCategory(event.target.value);
@@ -52,17 +55,36 @@ const NewProductForm = () => {
         setDescription(event.target.value);
     };
 
+    const submitHandler = async (data) => {
+        const formData = new FormData();
+        formData.append("name", name);
+        formData.append("desc", description);
+        formData.append("price", price);
+        formData.append("discount",0);
+        formData.append("stock", stock);
+        formData.append("categoryId",category);
+        formData.append("brandId", brand);
+        formData.append("images", data.file[0]);
+
+        for (var p of formData) {
+            console.log(p);
+          }
+
+        await axios.post("http://localhost:8080/product/", { body: formData })
+        .then(response => console.log(response))
+    };
+
     return (
         <>
         <Box display="flex" alignItems="center" justifyContent="center"><Typography variant="h5">Add new Product</Typography></Box>        
             <Box display="flex" alignItems="center" justifyContent="center">
-                <form>
+                <form encType="multipart/form-data" onSubmit={handleSubmit(submitHandler)}>
                     <TextField
                     style={{ width: "200px", margin: "5px" }}
                     type="text"
                     label="Product name"
                     variant="outlined"
-                    value={product.name || name}
+                    value={ name }
                     onChange={handleChangeName}
                     required
                     />
@@ -90,7 +112,7 @@ const NewProductForm = () => {
                         <Select
                         labelId="demo-simple-select-standard-label"
                         id="demo-simple-select-standard"
-                        value={product.brand || brand}
+                        value={ brand }
                         onChange={handleChangeBrand}
                         label="Brand"
                         >
@@ -110,7 +132,7 @@ const NewProductForm = () => {
                     type="number"
                     label="Stock"
                     variant="outlined"
-                    value={product.stock || stock}
+                    value={ stock }
                     onChange={handleChangeStock}
                     required
                     />
@@ -122,7 +144,7 @@ const NewProductForm = () => {
                     label="Price"
                     placeholder='Price'
                     variant="outlined"
-                    value={product.price || price}
+                    value={ price }
                     onChange={handleChangePrice}
                     required
                     />
@@ -131,13 +153,14 @@ const NewProductForm = () => {
                         aria-label="empty textarea"
                         placeholder="Description"
                         style={{ width: 210, height: 100 }}
-                        value={product.description || description}
+                        value={ description }
                         onChange={handleChangeDescription}
                         required
                     />
                     <br />
                     <input
                     style={{ width: "300px", margin: "5px" }}
+                    {...register("file")}
                     type="file"
                     accept="image/png, image/jpeg"
                     multiple
@@ -146,10 +169,10 @@ const NewProductForm = () => {
                     />
                     <br/>
                     <Box width='200px' margin='5px'  display='flex' alignItems='center' justifyContent='space-between'>                    
-                    <Button disabled={id} variant="contained" color="primary">
+                    <Button type='submit' disabled={id} variant="contained" color="primary">
                         Add
                     </Button>
-                    <Button disabled={!id} variant="contained" color="primary">
+                    <Button onSubmit={handleSubmit(submitHandler)} disabled={!id} variant="contained" color="primary">
                         Save
                     </Button>
                     </Box>
